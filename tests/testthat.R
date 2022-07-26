e818b5bc7be4cf42c17fb022e75d96bace73c55a
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2022-07-26 10:07:00 +0200

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,14 +1,4 @@
-pkg_name <- "crmPack"
-if (requireNamespace("testthat", quietly = TRUE)) {
-  library(testthat)
-  if (testthat:::on_ci()) {
-    reporter <- MultiReporter$new(list(
-      CheckReporter$new(),
-      JunitReporter$new(file = "junit-result.xml")
-    ))
-    test_results <- test_check(pkg_name, reporter = reporter)
-    saveRDS(test_results, "unit_testing_results.rds")
-  } else {
-    test_check(pkg_name)
-  }
-}
+library("testthat")
+library("crmPack")
+
+test_check("crmPack")
